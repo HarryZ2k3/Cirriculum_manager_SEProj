393cfd0bc27f3fc0844bddb8c5cb21db7a17083f
my003 <thaomypc2003@gmail.com>
2024-03-19 17:30:55 +0700

    push

--- .history/frontend/src/routes/Home/Home_20240319170322.jsx
+++
@@ -1,8 +0,0 @@
-import Transition from '../../utils/Transition'
-export default function Home() {
-	return (
-		<Transition className="flex justify-center items-center flex-col">
-            
-		</Transition>
-	)
-}
