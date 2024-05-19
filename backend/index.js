e44aa00c0a4afd070090e6440991d309c8b82055
HungNguywn321 <154654212+HungNguywn321@users.noreply.github.com>
2024-05-19 16:27:46 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,8 @@ app.get('/users/:username/:SemesterNumber/:Year',db.getGrades);
 app.get('/users/:username',db.GetInfoCourse);
 app.get('/users/:username/:Password',db.GetID);
 app.get('/users/:username/:SemesterNumber/:SemesterYear/:CourseName',db.GradesChart);
-app.get('/credits/:username', db.GetCredit);
+app.get('/credits/:username', db.GetCredit); // lấy tín chỉ tất cả sem
+app.get('/credits/:username/:SemesterNumber/:SemesterYear', db.CreditEachSem); //lấy tín chỉ từng semester
   
 
 app.listen(port, () => {
